336255c04a7cd973ab8e0c67b226d8b767abcb48
dongwon kang <dw3624@gmail.com>
2024-03-20 20:38:12 +0900

    feat: project structure 구성

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,20 +1,33 @@
 import { resolve } from 'path'
-import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 
 export default defineConfig({
   main: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  preload: {
-    plugins: [externalizeDepsPlugin()]
-  },
-  renderer: {
+    plugins: [externalizeDepsPlugin()],
     resolve: {
       alias: {
-        '@renderer': resolve('src/renderer/src')
-      }
+        '@/lib': resolve('src/main/lib'),
+        '@shared': resolve('src/shared'),
+      },
     },
-    plugins: [react()]
-  }
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    assetsInclude: 'src/renderer/assets/**',
+    resolve: {
+      alias: {
+        '@renderer': resolve('src/renderer/src'),
+        '@shared': resolve('src/shared'),
+        '@/hooks': resolve('src/renderer/src/hooks'),
+        '@/assets': resolve('src/renderer/src/assets'),
+        '@/store': resolve('src/renderer/src/store'),
+        '@/components': resolve('src/renderer/src/components'),
+        '@/mocks': resolve('src/renderer/src/mocks'),
+      },
+    },
+    plugins: [react()],
+  },
 })
